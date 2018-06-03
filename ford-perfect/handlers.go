11f990fe6d7e3130caf1e80d565e1c6723aa6ef8
Xennis <code@xennis.org>
2018-06-03 14:02:11 +0200

    Add gRPC to service

--- ford-perfect/handlers.go
+++ ford-perfect/handlers.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	pb "github.com/Xennis/ford-prefect-bot/telegram-bot-api"
+	"github.com/golang/protobuf/ptypes/empty"
+	"golang.org/x/net/context"
+)
 
 func readinessProbeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -8,4 +15,9 @@ func readinessProbeHandler(w http.ResponseWriter, r *http.Request) {
 
 func livenessProbeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
+}
+
+func (s *server) ReceiveUpdates(ctx context.Context, in *pb.Update) (*empty.Empty, error) {
+	fmt.Printf("Receive update: %v", in.UpdateId)
+	return new(empty.Empty), nil
 }
--- ford-perfect/main.go
+++ ford-perfect/main.go
@@ -2,14 +2,45 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
+
+	pb "github.com/Xennis/ford-prefect-bot/telegram-bot-api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
-func main() {
+type server struct{}
 
+func main() {
+	errRPC := make(chan error)
+	errHTTP := make(chan error)
+	go listenRPC(errRPC)
+	go listenHTTP(errHTTP)
+
+	select {
+	case err := <-errRPC:
+		log.Fatalf("Exited rpc with error: %s", err.Error())
+	case err := <-errHTTP:
+		log.Fatalf("Exited http with error: %s", err.Error())
+	}
+}
+
+func listenRPC(errChan chan error) {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		errChan <- err
+		return
+	}
+	s := grpc.NewServer()
+	pb.RegisterTelegramBotApiServer(s, &server{})
+	reflection.Register(s) // Register reflection service on gRPC server.
+	errChan <- s.Serve(lis)
+}
+
+func listenHTTP(errchan chan error) {
 	mux := http.NewServeMux()
 	mux.Handle("/ready", http.HandlerFunc(readinessProbeHandler))
 	mux.Handle("/healthz", http.HandlerFunc(livenessProbeHandler))
-
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	errchan <- http.ListenAndServe(":8080", mux)
 }
