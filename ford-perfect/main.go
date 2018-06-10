dc5da04d902a9eb151adb6fb3a22641f1a1e092e
Xennis <code@xennis.org>
2018-06-10 19:28:10 +0200

    Add Stackdriver logging

--- ford-perfect/main.go
+++ ford-perfect/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"cloud.google.com/go/logging"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+var serviceLogger *logging.Logger
+
 func main() {
-	log.Printf("Start service")
+	ctx := context.Background()
+
+	loggingClient, err := logging.NewClient(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"))
+	if err != nil {
+		log.Fatalf("Could not create logger: %v", err)
+	}
+	defer loggingClient.Close()
+	serviceLogger = loggingClient.Logger("ford-perfect")
+	serviceLogger.Log(logging.Entry{Payload: "Start service"})
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
